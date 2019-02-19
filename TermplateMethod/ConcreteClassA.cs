3a281ea564d1accfc2980f093edf0d66117ec482
Darlan Gagliotto <dlg_d2@hotmail.com>
2019-02-18 22:25:51 -0300

    TemplateMethod

---
+++ TermplateMethod/ConcreteClassA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace TermplateMethod
+{
+    class ConcreteClassA : AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+        }
+
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+        }
+    }
+}
---
+++ TermplateMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TermplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass classAbstract_A = new ConcreteClassA();
+            classAbstract_A.TemplateMethod();
+
+            AbstractClass classAbstract_B = new ConcreteClassB();
+            classAbstract_B.TemplateMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
