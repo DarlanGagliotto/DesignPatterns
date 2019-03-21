bae8dfd10c1003a4d7dc62c3d8a7b033a8fc2392
Darlan Gagliotto <dlg_d2@hotmail.com>
2019-03-20 21:25:54 -0300

    SingleTon

---
+++ SingleTon/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleTon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SingleTon jogador1 = SingleTon.GetInstanciaBola;
+            jogador1.Mensagem("Jogador 1 : Bola no centro do campo!");
+
+            SingleTon jogador2 = SingleTon.GetInstanciaBola;
+            jogador2.Mensagem("Jogador 2 : Recebe a bola do jogador 1!");
+
+            SingleTon jogador3 = SingleTon.GetInstanciaBola;
+            jogador3.Mensagem("Jogador 3 : Cabeceia o lançamento do jogador 2!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingleTon/SingleTon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SingleTon
+{
+    public sealed class SingleTon
+    {
+        private static SingleTon instanciaBola = null;
+
+        public static SingleTon GetInstanciaBola
+        {
+            get
+            {
+                if (instanciaBola == null)
+                {
+                    instanciaBola = new SingleTon();
+
+                    Console.WriteLine("Bola em jogo!");
+                }
+
+                return instanciaBola;
+            }
+        }
+
+        public void Mensagem(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
