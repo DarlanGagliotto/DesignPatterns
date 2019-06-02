7171b0e705e77e5dea93a30762f2336de890e8cf
Darlan Gagliotto <dlg_d2@hotmail.com>
2019-06-02 10:51:22 -0300

    Interpreter

---
+++ Interpreter/MilharesExpresao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class MilharesExpresao : Expressao
+    {
+        public override string Cinco()
+        {
+            return "D";
+        }
+
+        public override int Multiplicar()
+        {
+            return 1000;
+        }
+
+        public override string Nove()
+        {
+            return " ";
+        }
+
+        public override string Quatro()
+        {
+            return " ";
+        }
+
+        public override string Um()
+        {
+            return "M";
+        }
+    }
+}
