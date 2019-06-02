d0735de7234d6e2e2a2d26e05af2a68ff815c7a5
Darlan Gagliotto <dlg_d2@hotmail.com>
2019-06-02 09:49:43 -0300

    Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList student = new SortedList();
+            student.Add("Paulo");
+            student.Add("Zeca");
+            student.Add("Juca");
+            student.Add("Antonio");
+            student.Add("Carla");
+            student.Add("Joana");
+            student.Add("Maria");
+
+            student.SetSortStrategy(new QuickSort());
+            student.Sort();
+
+            student.SetSortStrategy(new ShellSort());
+            student.Sort();
+
+            student.SetSortStrategy(new MergeSort());
+            student.Sort();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strategy/SortedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class SortedList
+    {
+        private List<string> _List = new List<string>();
+        private SortStrategy _ShellSort;
+
+        public void SetSortStrategy(SortStrategy sortStrategy)
+        {
+            this._ShellSort = sortStrategy;
+        }
+
+        public void Add(string name)
+        {
+            _List.Add(name);
+        }
+
+        public void Sort()
+        {
+            _ShellSort.Sort(_List);
+
+            foreach (var item in _List)
+            {
+                Console.WriteLine(" " + item);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
